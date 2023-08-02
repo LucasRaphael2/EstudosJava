8629c9ea5735453fd475bdffffba9668f50b9d6f
Lucas Braga <lucas26raphael@hotmail.com>
2023-08-02 00:26:05 -0300

    alterações

--- estudosBackEnd/src/estudosBackEnd/Lanchonete.java
+++ estudosBackEnd/src/estudosBackEnd/Lanchonete.java
@@ -1,9 +1,25 @@
 package estudosBackEnd;
 
+import java.util.Scanner;
+
 public class Lanchonete {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		System.out.printf("Olá eu sou max, sejam muito bem-vindos a lanchonete do GORDÃO.%n%n");
+		System.out.printf(" Aqui está o cardapio para você dar uma olhada%n%n");
+		System.out.println("-----------------CARDÁPIO-----------------");
+		System.out.printf("%nBEBIDAS:%n%n1 - Guaraná Lata R$ 4,50 %n2 - Coca-Cola Lata R$ 5,00 %n3 - Sprite Lata R$ 4,00 %n4 - Pepsi Lata R$ 4,00%n5 - Fanta Lata R$ 4,50 %n%n");
+		System.out.printf("SANDUICHES:%n%n1 - X-Salada%n2 - Bauru%n3 - Minuano%n4 - X-Bacon%n5 - X-Calabresa%n6 - X-Tudo%n%n");
+		System.out.printf("PASSAPORTES/HOT DOGS:%n%n1 - Passaporte de misto%n2 - Passaporte de frango%n3 - Passaporte d carne%n4 - Passaporte de coração%n5 - Passaporte de camarão%n6 - Passaporte de queijo coalho%n%n");
+		System.out.printf("BATATAS:%n%n1 - Batata pequena%n2 - Batata média%n3 - Batata grande%n4 - Batata pequena com bacon%n5 - Batata média com bacon%n6 - Batata granda com bacon%n%n");
+		System.out.printf("ACOMPANHAMENTOS:%n%n1 - Bacon%n2 - Cheddar%n3 - Cream Cheese%n4 - Catupiry%n%n");
+		
+		
+		
+		
+		in.close();
 
 	}
 
