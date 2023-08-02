381feaf4edd03e8a79016b0ee58b6883385d3d20
Lucas-Braga04 <121194310+LucasRaphael2@users.noreply.github.com>
2023-08-02 01:35:55 -0300

    alterações

--- estudosBackEnd/src/estudosBackEnd/Lanchonete.java
+++ estudosBackEnd/src/estudosBackEnd/Lanchonete.java
@@ -10,10 +10,14 @@ public class Lanchonete {
 		System.out.printf("Olá eu sou max, sejam muito bem-vindos a lanchonete do GORDÃO.%n%n");
 		System.out.printf(" Aqui está o cardapio para você dar uma olhada%n%n");
 		System.out.println("-----------------CARDÁPIO-----------------");
-		System.out.printf("%nBEBIDAS:%n%n1 - Guaraná Lata R$ 4,50 %n2 - Coca-Cola Lata R$ 5,00 %n3 - Sprite Lata R$ 4,00 %n4 - Pepsi Lata R$ 4,00%n5 - Fanta Lata R$ 4,50 %n%n");
+		System.out.printf("%nBEBIDAS:%n%nLATA:%n1 - Guaraná R$ 4,50 %n2 - Coca-Cola R$ 5,00 %n3 - Sprite R$ 4,00 %n4 - Pepsi R$ 4,00%n5 - Fanta R$ 4,50%n%nLITRO:%n1 - Guaraná R$ 5,00%n2 - Coca-Cola R$ 6,00%n3 - Sprite R$ 5,00%n4 - Pepsi R$ 4,50%n5 - Fanta R$ 5,00%n%n");
+		
 		System.out.printf("SANDUICHES:%n%n1 - X-Salada%n2 - Bauru%n3 - Minuano%n4 - X-Bacon%n5 - X-Calabresa%n6 - X-Tudo%n%n");
+		
 		System.out.printf("PASSAPORTES/HOT DOGS:%n%n1 - Passaporte de misto%n2 - Passaporte de frango%n3 - Passaporte d carne%n4 - Passaporte de coração%n5 - Passaporte de camarão%n6 - Passaporte de queijo coalho%n%n");
+		
 		System.out.printf("BATATAS:%n%n1 - Batata pequena%n2 - Batata média%n3 - Batata grande%n4 - Batata pequena com bacon%n5 - Batata média com bacon%n6 - Batata granda com bacon%n%n");
+		
 		System.out.printf("ACOMPANHAMENTOS:%n%n1 - Bacon%n2 - Cheddar%n3 - Cream Cheese%n4 - Catupiry%n%n");
 		
 		
--- learningJavafx/src/application/Main.java
+++ learningJavafx/src/application/Main.java
@@ -1,18 +1,19 @@
-package application;
-	
+package learningJavafx;
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
 		
 		Pane root = FXMLLoader.load(getClass().getResource("FirstProject.fxml"));
-		Scene scene = new Scene(root,400,400);
+		Scene scene = new Scene(root,300,300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
