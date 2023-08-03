06aa9a51fafbe8328d3998ad81a574c575838d5f
Lucas-Braga04 <121194310+LucasRaphael2@users.noreply.github.com>
2023-08-02 21:59:32 -0300

    .

--- learningJavafx/src/application/Main.java
+++ learningJavafx/src/application/Main.java
@@ -1,25 +1,24 @@
 package learningJavafx;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		
-		Pane root = FXMLLoader.load(getClass().getResource("FirstProject.fxml"));
-		Scene scene = new Scene(root,300,300);
+		AnchorPane root = AnchorPane FXMLLoader.load(getClass().getResource("FirstProject.fxml"));
+		Scene scene = new Scene(root, 300, 300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch(Main.class, args);
 	}
 }
