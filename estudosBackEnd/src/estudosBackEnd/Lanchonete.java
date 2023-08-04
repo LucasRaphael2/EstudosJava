4907f73b7e283eb65eadc11469368f38e5a66277
Lucas Braga <121194310+LucasRaphael2@users.noreply.github.com>
2023-08-04 17:02:18 -0300

    alterações

--- estudosBackEnd/src/estudosBackEnd/Lanchonete.java
+++ estudosBackEnd/src/estudosBackEnd/Lanchonete.java
@@ -8,19 +8,27 @@ public class Lanchonete {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.printf("Olá eu sou max, sejam muito bem-vindos a lanchonete do GORDÃO.%n%n");
+		
 		System.out.printf(" Aqui está o cardapio para você dar uma olhada%n%n");
+		
 		System.out.println("-----------------CARDÁPIO-----------------");
-		System.out.printf("%nBEBIDAS:%n%nLATA:%n1 - Guaraná R$ 4,50 %n2 - Coca-Cola R$ 5,00 %n3 - Sprite R$ 4,00 %n4 - Pepsi R$ 4,00%n5 - Fanta R$ 4,50%n%nLITRO:%n1 - Guaraná R$ 5,00%n2 - Coca-Cola R$ 6,00%n3 - Sprite R$ 5,00%n4 - Pepsi R$ 4,50%n5 - Fanta R$ 5,00%n%n");
 		
-		System.out.printf("SANDUICHES:%n%n1 - X-Salada%n2 - Bauru%n3 - Minuano%n4 - X-Bacon%n5 - X-Calabresa%n6 - X-Tudo%n%n");
+		System.out.printf("%nBEBIDAS:%n%nLATA:%n1 - Sprite R$ 4,00 %n2 - Pepsi R$ 4,00%n3 - Guaraná R$ 4,50 %n4 - Fanta R$ 4,50 %n5 - Coca-Cola R$ 5,00%n%nLITRO:%n1 - Guaraná R$ 5,00%n2 - Coca-Cola R$ 6,00%n3 - Sprite R$ 5,00%n4 - Pepsi R$ 4,50%n5 - Fanta R$ 5,00%n%n");
 		
-		System.out.printf("PASSAPORTES/HOT DOGS:%n%n1 - Passaporte de misto%n2 - Passaporte de frango%n3 - Passaporte d carne%n4 - Passaporte de coração%n5 - Passaporte de camarão%n6 - Passaporte de queijo coalho%n%n");
+		System.out.printf("PRONTOS: %n%n");
 		
-		System.out.printf("BATATAS:%n%n1 - Batata pequena%n2 - Batata média%n3 - Batata grande%n4 - Batata pequena com bacon%n5 - Batata média com bacon%n6 - Batata granda com bacon%n%n");
+		System.out.printf("SANDUICHES:%n%n1 - X-Salada R$ 6,00%n2 - Bauru R$ 7,00%n3 - Minuano R$ 8,50%n4 - X-Bacon R$ 10,50%n5 - X-Calabresa R$ 12,00%n6 - X-Tudo R$ 17,00%n%n");
 		
-		System.out.printf("ACOMPANHAMENTOS:%n%n1 - Bacon%n2 - Cheddar%n3 - Cream Cheese%n4 - Catupiry%n%n");
+		System.out.printf("PASSAPORTES/HOT DOGS:%n%n1 - Passaporte de queijo coalho R$ 6,00%n2 - Passaporte de misto R$ 7,00%n3 - Passaporte de frango R$ 8,50%n4 - Passaporte de carne R$ 9,00%n5 - Passaporte de coração R$ 10,00%n6 - Passaporte de camarão R$ 12,00%n%n");
 		
+		System.out.printf("BATATAS:%n%n1 - Batata pequena R$ 5,00%n2 - Batata média R$ 7,00%n3 - Batata grande R$ 10,00%n4 - Batata pequena com bacon 7,50%n5 - Batata média com bacon R$ 9,50%n6 - Batata granda com bacon R$ 12,50%n%n");
 		
+		System.out.printf("ACOMPANHAMENTOS EXTRAS:%n%n1 - Bacon R$ 2,50%n2 - Cheddar R$ 2,00%n3 - Cream Cheese R$ 2,00%n4 - Catupiry R$ 2,00%n%n");
+		
+		System.out.printf("MONTAVEIS: %n%n");
+		
+		System.out.printf("PÃES:%n%n1 - Seda R$ 1,50%n2 - Bola R$ 1,50%n3 - Italiano R$ 2,50%n4 - 9 grãos R$ 2,50%n%n");
+		System.out.printf("CARNES:%n%n1 - Carne em tiras%n2 - ");
 		
 		
 		in.close();
