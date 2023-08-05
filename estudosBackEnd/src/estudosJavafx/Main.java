cccbe32ce1ded9242ea550e322ed923fa22a96b7
Lucas Braga <lucas26raphael@hotmail.com>
2023-08-05 02:52:21 -0300

    alterações

--- estudosBackEnd/src/estudosJavafx/Main.java
+++ estudosBackEnd/src/estudosJavafx/Main.java
@@ -1,23 +1,25 @@
-package application;
+package estudosJavafx;
 
 import java.io.IOException;
-import java.security.PublicKey;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Test {
-	public void start(Stage primaryStage) throws IOException{
+public class Main extends Application {
+
+	public void start(Stage primaryStage) throws IOException {
 		
 		AnchorPane root = FXMLLoader.load(getClass().getResource("FirstProject.fxml"));
 		Scene scene = new Scene(root, 300, 300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 
 }
